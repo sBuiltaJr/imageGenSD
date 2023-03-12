a78891a11ec4be85510e8b22516ec51e51639e38
sBuiltaJr <106572743+sBuiltaJr@users.noreply.github.com>
2023-03-12 12:59:50 -0700

    Capturing expanded project planning and config options, subject to change.

--- imageGenSD.py
+++ imageGenSD.py
@@ -8,6 +8,11 @@ import discord as dis
 import logging as log
 import os
 
+#The great thing about dictionaries defined at the package level is their global
+#(public-like) capability, avoiding constant passing down to 'lower' defs. 
+#Static data only, no file objects or similar (or else!).
+params = {'cfg'       : 'config.json',
+          'bot_token' : ''}
 IGSD_version = '0.0.1'
 
 
@@ -17,4 +22,4 @@ IGSD_version = '0.0.1'
 
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())

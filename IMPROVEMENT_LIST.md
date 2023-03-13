a6d80ee6ee3a0d598905266acd6679da503bc779
sBuiltaJr <106572743+sBuiltaJr@users.noreply.github.com>
2023-03-12 19:05:58 -0700

    Users/sbuilta/feature/projinit (#1)
    * Continuing initial code groundwork.
    
    * It does help to put the files in the right repo though.
    
    * Capturing expanded project planning and config options, subject to change.
    
    * Approaching first integration of bod->Discord.  bot->SD still to come.
    
    * Temporarily untracking the git creds instead of working in a different branch.  Will resume as command-line args allow redirected cred files.
    
    * Minor fixes.
    
    * First working login run of the bot (with credential values removed).
    
    * Adding a CODEOWNERS file for gitHub management.

---
+++ IMPROVEMENT_LIST.md
@@ -0,0 +1,27 @@
+# Overview
+This is a list of possible upgrades/updates that may be added to the project.
+They're listed here so as to not be forgotten.  Neither list is in priority
+order. [The discord.py github](https://github.com/Rapptz/discord.py/tree/master/examples) has many examples for reference.
+
+## Bug-fixes
+(These items have higher priority but aren't guaranteed to be first).
+
+## Investigations
+(Items that may not be bugs but should still be investigated)
+- Determine why the discord.py examples always requrie hard-coding a guild. (should be auto-detectable?)
+- Determine how to properly background the task (discord.py has some examples, may not work as needed?)
+
+## Planned
+- Allow a bot to manage multiple guilds.
+- Allow a bot to shard.
+- Crash recovery.
+- Guild info command (for setting parameters).
+- Argument sanitization/limiting (discord knows as transformers).
+- Add user-supplied paths for config/credential files.
+- Implement better logging configuration/customization by using the logging config files.
+- Multitasking (including separate logging tasks).
+- zipping logs to reduce increase logged information.
+- Help menu via messaging the bot.
+
+## Unplanned
+- weeb shit
---
+++ imageGenSD.py
@@ -0,0 +1,109 @@
+#Manages user requests through Discord to generate images with a Stable
+#Diffusion model filepath supplied in the config.json file.  Cannot prevent
+#Specific types of images, like NSFW, from being generated.
+
+
+#####  Imports  #####
+
+import asyncio
+import discord as dis
+from discord import app_commands as dac
+import gzip
+import logging as log
+import logging.handlers as lh
+import json
+import os
+import shutil as sh
+
+#####  Package Variables  #####
+
+#The great thing about package-level dictionaries is their global (public-like)
+#capability, avoiding constant passing down to 'lower' defs. 
+#Static data only, no file objects or similar (or else!).
+creds = {}
+default_params = {'cfg'       : 'config.json',
+                  'cred'      : 'credentials.json',
+                  'bot_token' : ''}
+IGSD_version = '0.0.1'
+params = {}
+
+#####  Package Classes  #####
+
+class IGSDClient(dis.Client):
+    def __init__(self, *, intents: dis.Intents):
+        super().__init__(intents=intents)
+        self.tree = dac.CommandTree(self)
+
+    async def setup_hook(self):
+        # This copies the global commands over to your guild.
+        self.tree.copy_global_to(guild=dis.Object(creds['guild_id']))
+        await self.tree.sync(guild=dis.Object(creds['guild_id']))
+
+intents = dis.Intents.default()
+IGSD_client = IGSDClient(intents=intents)
+
+#####  Package Functions  #####
+
+@IGSD_client.event
+async def on_ready():
+    print(f'Logged in as {IGSD_client.user} (ID: {IGSD_client.user.id})')
+    print('------')
+    
+@IGSD_client.tree.command()
+async def hello(interaction: dis.Interaction):
+    """A test echo command to verify basic discord functionality.
+
+       Input : None.
+
+       Output : None.
+    """
+    await interaction.response.send_message(f'Hi, {interaction.user.mention}')
+    
+#####  main  #####
+
+def startup():
+    """Updates the global dictionary with the supplied configuration, if it
+       exists, and starts the program.
+
+       Input : None (yet).
+
+       Output : None.
+    """
+    global params
+    global creds
+    
+    #This will be modified in the future to accept super-supplied paths.
+    #This file must be loaded prior to the logger to allow for user-provided
+    #options to be passed to the Logger.  Thus it must have special error
+    #handling outside of the logger class.
+    with open(default_params['cfg']) as json_file:
+        params = json.load(json_file)
+        
+    disLog = log.getLogger('discord')
+    disLog.setLevel(params['log_lvl'])
+
+    logHandler = lh.RotatingFileHandler(
+        filename=params['log_name'],
+        encoding=params['log_encoding'],
+        maxBytes=int(params['max_bytes']),
+        backupCount=int(params['log_file_cnt']),
+    )
+    
+    formatter = log.Formatter(
+        '[{asctime}] [{levelname:<8}] {name}: {message}', 
+        params['date_fmt'],
+        style='{'
+    )
+    logHandler.setFormatter(formatter)
+    disLog.addHandler(logHandler)
+    
+    #This will be modified in the future to accept super-supplied paths.
+    with open(default_params['cred']) as json_file:
+        creds = json.load(json_file)
+
+    IGSD_client.run(creds['bot_token'])
+
+
+if __name__ == '__main__':
+    startup()
+#asyncio.run(startup())

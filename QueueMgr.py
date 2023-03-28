dec1dfd518216632c8307f5d6d1c1488426a16f4
sBuiltaJr <106572743+sBuiltaJr@users.noreply.github.com>
2023-03-27 20:34:03 -0600

    Users/sbuilta/proj init backend (#2)
    * Saving files before trip.
    
    * Saving initial get test on URL + better (ugly) missing file handling.
    
    * The request works better if you give it a URL.
    
    * Converted to requests package because native python URL package can't handle JSON->URL conversions.  Actual PUt testing not fully verified.
    
    * I'd never commit something without testing it.
    
    * Fixed the PUT to at least generate files again.  Added full API params for validation (defaults weren't properly being set when params were excluded).
    
    * Sketched out some commands and how to handle a pool and rate limits.  Discord requries 3-second responses, forcing a job manager.
    
    * Interrim file as I work out a manager for slash commands.
    
    * Getting closer to a properly multithreaded solution.  Discord's timeout sucks.
    
    * Checkpoint before cleaning code.
    
    * Cleaned enough to start the runtime cleanup.
    
    * Finally getting close to initial debugging of the put system.
    
    * A mostly cleaned version of the first working loop around the internal task splitting.  Still need to push a test image to SD and determine why the exit hangs.
    
    * finally got the message posting in the right thread, preparing for a merge to mainline.
    
    * Hopefully final cleanup before a merge into main.
    
    * there's always typos and formatting issues.
    
    ---------
    
    Co-authored-by: Random <nope@GDIgit.com>

---
+++ QueueMgr.py
@@ -0,0 +1,180 @@
+#Manages the job queue for processing IGSD images. Also handles reate limits
+#nad other checks, including tracking guilds.
+#
+#A seaprate manager is reqruied by Discord becuase all slash commands have a
+#Hard 3-second timeout.  Even modern HW is not yet able to meet this demand.
+
+
+#####  Imports  #####
+
+import logging as log
+import multiprocessing as mp
+import requests as req
+from urllib.parse import urljoin
+
+jobs = {}
+#####  Package Functions  #####
+  
+#####  Manager Class  #####
+
+class Manager:
+
+    def __init__(self, loop, manager_id: int, opts: dict):
+        """Manages job request queueing and tracks relevant discord context,
+           such as poster, Guild, channel, etc.  The Manager gets this from the
+           caller so different Managers could have different settings.
+
+           Input: self - Pointer to the current object instance.
+                  loop - The asyncio event loop this manager posts to.
+                  manager_id - The current Manager's ID, assigned by the caller.
+                  opts - A dictionary of options, like cooldowns.
+              
+           Output: None - Throws exceptions on error.
+        """
+        self.disLog     = log.getLogger('discord')
+        self.id         = manager_id
+        self.keep_going = True
+        self.post_loop  = loop
+        #It's possible all opts are provided directly from config.json,
+        #requiring them to be cast appropriately for the manager.  This also
+        #allows the caller to never have to worry about casting the types
+        #correctly for a config file and definition it doesn't own.
+        self.depth          = int(opts['depth'])
+        self.job_cooldown   = float(opts['job_cooldown'])
+        self.max_guilds     = int(opts['max_guilds'])
+        self.max_guild_reqs = int(opts['max_guild_reqs'])
+        self.post_cooldown  = float(opts['post_cooldown'])
+        self.web_url        = opts['webui_URL']
+        
+        #This may eventually be implemented as a concurrent futures ProcessPool
+        #to allow future versions to invoke workers across computers (e.g.
+        #subprocess_exec with TCP/UDP data to/from a set of remote terminals).
+        self.queue = mp.Queue(self.depth)
+        
+    def Add(self, request : dict):
+        """Passes queued jobs to the worker tasks.  Is effectively the 'main'
+           of the class.  Workers return the image prompt and queue object id
+           when compelte.  The Manager should post the result to the main thread
+           via a pipe to allow simultaneous handling of commands and responses.
+
+           Input: self - Pointer to the current object instance.
+                  request - sanitized data to potentially add to the queue.
+              
+           Output: None - Results are posted to a pipe.
+        """
+        global jobs
+        
+        if request['metadata']['id'] not in jobs:
+            jobs[request['metadata']['id']] = request
+            self.disLog.debug(f"Added new request to id {request['metadata']['id']}.")
+        else :
+            self.disLog.debug(f"Request id {request['metadata']['id']} alraedy exists!")
+            return
+        
+            #Else rate limit
+        #The Metadata can't be pickeled, meaning we can only send data through the queue.
+        self.queue.put(request['data'])
+    
+    def GetDefaultJobData(self) -> dict:
+        """Returns the default job settings that can be provided to an empty
+           query (or to reinitialize an object).
+
+           Input: None.
+
+           Output: opts - a dictionary of the default queue arguments.
+        """
+        return {
+        'enable_hr'           : False,
+        'denoising_strength'  : 0,
+        'firstphase_width'    : 0,
+        'firstphase_height'   : 0,
+        'hr_scale'            : 2,
+        'hr_upscaler'         : "string",
+        'hr_second_pass_steps': 0,
+        'hr_resize_x'         : 0,
+        'hr_resize_y'         : 0,
+        'prompt'              : "(exceptional, best aesthetic, best quality, masterpiece, extremely detailed:1.2), white dress, dress, short sleeves, strapless dress, frills, thighhigh stockings, black thighhighs, boots, red hair, long hair, medium breasts, blush, slight smile, painting, paintbrush, eyebrows visible through hair, standing, easel, paint, blue eyes, brown shoes, bangs, canvas (object), holding paintbrush, braid, braided hair, painting (object), bow, yellow bow, hands up, hair ornament, indoors, cute,",
+        'styles'              : ["string"],
+        'seed'                : -1, #Should really identify which seed IGSD used for itself.
+        'subseed'             : -1,
+        'subseed_strength'    : 0,
+        'seed_resize_from_h'  : -1,
+        'seed_resize_from_w'  : -1,
+        'sampler_name'        : "",
+        'batch_size'          : 1,
+        'n_iter'              : 1,
+        'steps'               : 70,
+        'cfg_scale'           : 22.0,
+        'width'               : 512,
+        'height'              : 512,
+        'restore_faces'       : False,
+        'tiling'              : False,
+        'do_not_save_samples' : False,
+        'do_not_save_grid'    : False,
+        'negative_prompt'     : " Negative prompt: lowres, ((bad anatomy)), ((bad hands)), text, missing finger, extra digits, fewer digits, blurry, ((mutated hands and fingers)), (poorly drawn face), ((mutation)), ((deformed face)), (ugly), ((bad proportions)), ((extra limbs)), extra face, (double head), (extra head), ((extra feet)), monster, logo, cropped, worst quality, jpeg, humpbacked, long body, long neck, ((jpeg artifacts)), deleted, old, oldest, ((censored)), ((bad aesthetic)), (mosaic censoring, bar censor, blur censor),  watermark, (low quality, worst quality:1.4), (bad anatomy), extra digit, fewer digits, (extra arms:1.2), bad hands, by (bad-artist:0.6), bad-image-v2-39000, NSFW, nipples, loli, child, children, shota, boy, male, men, man",
+        'eta'                 : 0,
+        's_churn'             : 0,
+        's_tmax'              : 0,
+        's_tmin'              : 0,
+        's_noise'             : 1,
+        'override_settings'   : {},
+        'override_settings_restore_afterwards': True,
+        'script_args'         : [],
+        'sampler_index'       : "DPM++ SDE Karras",
+        'script_name'         : "",
+        'send_images'         : True,
+        'save_images'         : True, #Should probably limit ability to run test command
+        'alwayson_scripts'    : {}
+        }
+        
+    def PutRequest(self) :
+        """Should be instantiated as an independent proecss for putting and
+           getting data from the SD werver.  Results are provided back to the
+           main IGSD thread viathe supplied event loop.  Has no knowledge of
+           Guilds or how to post the provided image to the requestor.
+
+            Input: pipe - where to return the SD image gen result.
+                  
+            Output: None - Throws exceptions on error.
+        """
+        global jobs
+        
+        while self.keep_going:
+            request = self.queue.get()
+            jres = {}
+            
+            #Have a special check for the GET test, which doesn't expect to get
+            #any data from an actual job.
+            if request['id'] == "testgetid":
+                self.disLog.debug(f"Performing GET test of URL: {self.web_url}.")
+                result = req.get(url=urljoin(self.web_url, '/sdapi/v1/memory'), timeout=5)
+            
+            else:
+                self.disLog.info(f"Starting put to SD server at {self.web_url}.")
+                result  = req.post(url=urljoin(self.web_url, '/sdapi/v1/txt2img'), json=request['post'])
+                jres    = result.json()
+
+            jres['status_code'] = result.status_code
+            jres['reason']      = result.reason
+            #Pop last to ensure a new request from the same ID can be added
+            #only after thier first request is completed.
+            job     = jobs.pop(request['id'])
+            jres   |= job['metadata']
+            job['metadata']['loop'].create_task(job['metadata']['poster'](msg=jres), name="reply")
+        return
+        
+    def Run(self):
+        """SHould spawn the process that puts job requests to the SD server.
+           The job task uses the provided pipe to post results, the manager 
+           only needs to wait for completion before starting another job.  Is
+           the freamework for multiple concurrent jobs, but will need to be
+           modified to actually support them.
+
+           Input: self - Pointer to the current object instance.
+              
+           Output: None - Results are posted to a pipe.
+        """
+        self.disLog.info(f"Queue Manager {self.id} starting workers.")
+        #This may, someday, need to be a proper multiprocessing queue.
+        #jobs = [QueueObject(x) for x in range(self.depth)], in a loop
+
--- imageGenSD.py
+++ imageGenSD.py
@@ -5,15 +5,21 @@
 
 #####  Imports  #####
 
-import asyncio
+import asyncio as asy
+import base64 as b64
 import discord as dis
 from discord import app_commands as dac
-import gzip
+import io
 import logging as log
 import logging.handlers as lh
 import json
+import multiprocessing as mp
 import os
-import shutil as sh
+import QueueMgr as qm
+import requests as req
+import threading as th
+import time
+from urllib.parse import urljoin
 
 #####  Package Variables  #####
 
@@ -24,20 +30,54 @@ creds = {}
 default_params = {'cfg'       : 'config.json',
                   'cred'      : 'credentials.json',
                   'bot_token' : ''}
-IGSD_version = '0.0.1'
+IGSD_version = '0.0.3'
 params = {}
+job_queue = ()
+worker = ()
 
 #####  Package Classes  #####
 
 class IGSDClient(dis.Client):
     def __init__(self, *, intents: dis.Intents):
+        """This command copies the global command set to a given Guild instance.
+
+            Input  : self - a reference to the current object.
+                     intents - what Discord intents are required to run the bot.
+
+            Output : None
+        """
+        self.disLog = log.getLogger('discord')
+        self.disLog.debug(f"Intents are: {intents}")
+        
         super().__init__(intents=intents)
         self.tree = dac.CommandTree(self)
 
     async def setup_hook(self):
-        # This copies the global commands over to your guild.
+        """Copies the global command set to a given Guild instance.
+
+            Input  : self - a reference to the current object.
+
+            Output : None
+        """
+        
+        #Replies should be managed in a separate task to allow the main UI to
+        #always be responsive, and allow the backend to process work independent
+        #of message posting.  It's more efficent and better separated.
+        self.loop = asy.get_running_loop()
+        
         self.tree.copy_global_to(guild=dis.Object(creds['guild_id']))
+        self.disLog.debug(f"Syncing Guild Tree to {creds['guild_id']}.")
+        
         await self.tree.sync(guild=dis.Object(creds['guild_id']))
+        
+    def GetLoop(self):
+        """Returns a reference to this client's asyncio event loop.
+
+            Input  : self - a reference to the current object.
+
+            Output : loop - the client's event loop.
+        """
+        return self.loop;
 
 intents = dis.Intents.default()
 IGSD_client = IGSDClient(intents=intents)
@@ -46,22 +86,147 @@ IGSD_client = IGSDClient(intents=intents)
 
 @IGSD_client.event
 async def on_ready():
-    print(f'Logged in as {IGSD_client.user} (ID: {IGSD_client.user.id})')
+    global job_queue
+    global worker
+    
+    disLog = log.getLogger('discord')
+    disLog.info(f'Logged in as {IGSD_client.user} (ID: {IGSD_client.user.id})')
+    
+    disLog.debug(f"Creating Queue Manager)")
+    job_queue = qm.Manager(loop=IGSD_client.GetLoop(),
+                           manager_id=1,
+                           opts=params['queue_opts'])
+    worker    = th.Thread(target=job_queue.PutRequest,
+                          name="Queue mgr",
+                          daemon=True)
+    worker.start()
+    
     print('------')
     
 @IGSD_client.tree.command()
 async def hello(interaction: dis.Interaction):
     """A test echo command to verify basic discord functionality.
 
-       Input : None.
+       Input  : None.
 
        Output : None.
     """
     await interaction.response.send_message(f'Hi, {interaction.user.mention}')
     
+        
+@IGSD_client.tree.command()
+async def testget(interaction: dis.Interaction):
+    """A test HTTP GET command to verify basic connection to the webui page.
+       Also tests the internal data path.
+
+       Input  : None.
+
+       Output : None.
+    """
+    disLog = log.getLogger('discord')
+
+    msg = { 'metadata' : {
+                'ctx'    : interaction,
+                'loop'   : IGSD_client.GetLoop(),
+                'poster' : Post,
+                'id'    : "testgetid"
+           },
+           'data' : {
+                #The repeat is due to unpickelable data in the metadata dict.
+                'id'    : "testgetid",
+                'post'  : {'empty'},
+                'reply' : "test msg"
+            }
+        }
+    disLog.debug(f"Posting test GET job {msg} to the queue") 
+    job_queue.Add(msg)
+    
+    try:
+        #This should proabbly be moved to a manager since there may eventually
+        #be multiple IP addresses.
+        response = req.get(url=params['queue_opts']['webui_URL'], timeout=5)
+    except Exception as err:
+        await interaction.response.send_message(f"Error sending GET request, got {err}!")
+        return
+    await interaction.response.send_message(f"Starting GET test.")
+
+@IGSD_client.tree.command()
+async def testpost(interaction: dis.Interaction):
+    """A test HTTP PUT command to verify basic connection to the webui page.
+
+       Input  : None.
+
+       Output : None.
+       
+       Note: All slash commands *MUST* respond in 3 seconds or be terminated.
+    """
+    disLog = log.getLogger('discord')
+    msg = { 'metadata' : {
+                'ctx'    : interaction,
+                'loop'   : IGSD_client.GetLoop(),
+                'poster' : Post,
+                'id'    : "testpostid"
+           },
+           'data' : {
+                #The repeat is due to unpickelable data in the metadata dict.
+                'id'    : "testpostid",
+                'post'  : job_queue.GetDefaultJobData()},
+                'reply' : ""
+            }
+    disLog.debug(f"Posting test PUT job {msg} to the queue") 
+    job_queue.Add(msg)
+    await interaction.response.send_message(f'Posted Test Message to work queue.')
+       
+async def Post(msg):
+    """Posts the query's result to Discord.  Runs in the main asyncio loop so
+       the manager can start the next job concurrently.
+
+        Input  : msg - a reference to the completed job.
+
+        Output : N/A.
+    """
+    embed = None
+    image = None
+    
+    if msg['status_code'] != 200:
+        embed = dis.Embed(title='Job Error:',
+                          description=f"Status code: {msg['status_code']} Reason: {msg['reason']}",
+                          color=0xec1802)
+                          
+        await msg['ctx'].channel.send(content=f"<@{msg['ctx'].user.id}>",
+                                      embed=embed)
+        
+    elif msg['id'] == 'testgetid':
+        #Maybe a futuer version will have a generic image to return and eliminate this clause.
+        embed = dis.Embed(title='Test GET successful: ',
+                          description=f"Status code: {msg['status_code']} Reason: {msg['reason']}",
+                          color=0x008000)
+
+        await msg['ctx'].channel.send(content=f"<@{msg['ctx'].user.id}>",
+                                      embed=embed)
+        
+    else:
+        embed = dis.Embed()
+        embed.add_field(name='Prompt', value=msg['parameters']['prompt'])
+        embed.add_field(name='Negative Prompt', value=msg['parameters']['negative_prompt'])
+        embed.add_field(name='Steps', value=msg['parameters']['steps'])
+        embed.add_field(name='Height', value=msg['parameters']['height'])
+        embed.add_field(name='Width', value=msg['parameters']['width'])
+        embed.add_field(name='Sampler', value=msg['parameters']['sampler_index'])
+        embed.add_field(name='Seed', value=msg['parameters']['seed'])
+        embed.add_field(name='CFG Scale', value=msg['parameters']['cfg_scale'])
+        embed.add_field(name='Highres Fix', value=msg['parameters']['enable_hr'])
+
+        for i in msg['images']:
+            image = io.BytesIO(b64.b64decode(i.split(",", 1)[0]))
+        
+        await msg['ctx'].channel.send(content=f"<@{msg['ctx'].user.id}>",
+                                      file=dis.File(fp=image,
+                                      filename='image.png') ,embed=embed)
+
 #####  main  #####
 
-def startup():
+def Startup():
     """Updates the global dictionary with the supplied configuration, if it
        exists, and starts the program.
 
@@ -71,8 +236,9 @@ def startup():
     """
     global params
     global creds
+    global job_queue
     
-    #This will be modified in the future to accept super-supplied paths.
+    #This will be modified in the future to accept user-supplied paths.
     #This file must be loaded prior to the logger to allow for user-provided
     #options to be passed to the Logger.  Thus it must have special error
     #handling outside of the logger class.
@@ -97,13 +263,38 @@ def startup():
     logHandler.setFormatter(formatter)
     disLog.addHandler(logHandler)
     
-    #This will be modified in the future to accept super-supplied paths.
-    with open(default_params['cred']) as json_file:
-        creds = json.load(json_file)
+    #This will be modified in the future to accept user-supplied paths.
+    try:
+        with open(default_params['cred']) as json_file:
+            creds = json.load(json_file)
 
+    except OSError as err:
+        disLog.critical(f"Can\'t load file from path {default_params['cred']}")
+        exit(1)
+    
+    #Start manager tasks
+    disLog.debug(f"Starting Bot client")
     IGSD_client.run(creds['bot_token'])
 
 
 if __name__ == '__main__':
-    startup()
-#asyncio.run(startup())
+    Startup()
+
+#Supported commands:
+#/flush:   clear queue and kill active jobs (if possible).  Needs Owern/Admin to run.
+#/Restart: Flush + recreates the queue objects.  Effectively restarts teh script.  Also requries Owner.
+#/Cancel:  Kills most recent request from the poster, if possible. 
+#Have the slash command inherently check rate limit against user ID.
+# Raete limiting:
+    # 1) Limit per guild (X requests in X seconds and Y total requests).
+        #Check limits before accepting job to queue.
+        #respond error if full
+        #Adds req. to guild dict (including making) and timestamp.
+            #Only need 'last added' tiemstamp, limiting input not output. (jobs only produce one file)
+        #Support X number of guidls in config.
+    # 2) Allow configruable options later (Owner/inviter config commands, also in JSON)
+    # 3) Check Guild limits against dict size storing data (unique dicts per guild)
+    # 4) Queue msut be deep enough to handle guild sizes (multi-queue probably too painful)
+    # 5) No limit on specific user spam (yet).  May not be neede dwit Guild limit.
+    # 6) Worker pool(of 1 for now, ac nfarm with expansion) pops form queue and reports guild ID when complete.
+    # 7) Handler deletes entries from guild dict.
